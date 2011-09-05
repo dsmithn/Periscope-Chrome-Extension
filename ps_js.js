c525a3ae53ba24ba7781f92ed351bea279b157f7
Destin Smith-Norris <destinsmithn@gmail.com>
2011-09-05 18:06:35 -0400

    Added jquery, moved search functoin to background tab

--- ps_js.js
+++ ps_js.js
@@ -1,38 +1,7 @@
-chrome.extension.sendRequest({'getTerms': true}, function(response) {
-	search(response.terms);
+chrome.extension.sendRequest({'searchBody': true, 'html': document.body.innerHTML}, function(response) {
+	console.log(response);
+	if(response.success) {
+		document.body.innerHTML = response.html;
+	}
 });
 
-
-function search(terms) {
-	var term = terms.toLowerCase();
-	var highlightStartTag = "<font style='color:blue; background-color:yellow;'>";
-	var highlightEndTag = "</font>";
-	
-	allTerms = term.split(',');
-
-	for(var n = 0; n < allTerms.length; n++) {
-		var newText = "";
-		var i = -1;
-		var bodyText = document.body.innerHTML;
-		var lcBodyText = document.body.innerHTML.toLowerCase();
-		term = allTerms[n];
-
-		while (bodyText.length > 0) {
-			i = lcBodyText.indexOf(term, i+1);
-			if (i < 0) {
-				newText += bodyText;
-				bodyText = "";
-			} else {
-				if (bodyText.lastIndexOf(">", i) >= bodyText.lastIndexOf("<", i)) {
-					if (lcBodyText.lastIndexOf("/script>", i) >= lcBodyText.lastIndexOf("<script", i)) {
-						newText += bodyText.substring(0, i) + highlightStartTag + bodyText.substr(i, term.length) + highlightEndTag;
-						bodyText = bodyText.substr(i + term.length);
-						lcBodyText = bodyText.toLowerCase();
-						i = -1;
-					}
-				}
-			}
-		}
-		document.body.innerHTML = newText;
-	}
-}
