a192daec0be018d543e489b7d3832f48459069ee
Destin Smith-Norris <destinsmithn@gmail.com>
2013-03-22 02:43:40 -0400

    not needed

--- ps_js.js
+++
@@ -1,7 +0,0 @@
-chrome.extension.sendRequest({'searchBody': true, 'html': document.body.innerHTML}, function(response) {
-	console.log(response);
-	if(response.success) {
-		document.body.innerHTML = response.html;
-	}
-});
-
