aa94ee34d56a00f80720e02ab51e28f197cc12d3
Destin Smith-Norris <destinsmithn@gmail.com>
2013-03-23 11:08:36 -0400

    saved searches

--- ps.js
+++ ps.js
@@ -1,29 +1,34 @@
 function saveSearch() {
-  var search = {};
-  search.val = $('#search').value;
-  search.site = $('#site').value;
-  saveTabData(tab, search);
+    var search = {};
+    search.val = $('#search').value;
+    search.site = $('#site').value;
+    saveTabData(tab, search);
 }
 
 function saveTabData(data) {
-  localStorage = data;  // OK to store data
+    localStorage = data; // OK to store data
 }
 
 function searchFor(searchVal) {
-    if(searchVal === "") return;
+    if (searchVal === "") return;
     count = 0;
     highlightWord(document.getElementsByTagName("body")[0], searchVal);
-    chrome.extension.sendMessage({type: "badge", text: count.toString()}, function(response) {console.log("sent")});
+    chrome.extension.sendMessage({
+        type: "badge",
+        text: count.toString()
+    }, function(response) {
+        console.log("sent")
+    });
 }
 
-console.log("ps.js loaded");
 var count = 0;
 chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
+
+function(request, sender, sendResponse) {
     if (request.type == "search") {
-      searchFor(request.term);
+        searchFor([request.term]);
     }
-  });
+});
 
 /* http://www.kryogenix.org/code/browser/searchhi/ */
 /* Modified 20021006 to fix query string parsing and add case insensitivity */
@@ -32,70 +37,55 @@ chrome.extension.onMessage.addListener(
 
 /* This script is by Stuart Langridge licensed under MIT license http://www.kryogenix.org/code/browser/licence.html */
 
-function highlightWord(node,word) {
-  // Iterate into this nodes childNodes
-  if (node.hasChildNodes) {
-    var hi_cn;
-    for (hi_cn=0;hi_cn<node.childNodes.length;hi_cn++) {
-      highlightWord(node.childNodes[hi_cn],word);
+function highlightWord(node, words) {
+    // Iterate into this nodes childNodes
+    if (node.hasChildNodes) {
+        var hi_cn;
+        for (hi_cn = 0; hi_cn < node.childNodes.length; hi_cn++) {
+            highlightWord(node.childNodes[hi_cn], words);
+        }
     }
-  }
-  
-  // And do this node itself
-  if (node.nodeType == 3) { // text node
-    tempNodeVal = node.nodeValue.toLowerCase();
-    tempWordVal = word.toLowerCase();
-    if (tempNodeVal.indexOf(tempWordVal) != -1) {
-      pn = node.parentNode;
-      // check if we're inside a "nosearchhi" zone
-      checkn = pn;
-      while (checkn.nodeType != 9 && 
-        checkn.nodeName.toLowerCase() != 'body') { 
-      // 9 = top of doc
-    if (checkn.className.match(/\bnosearchhi\b/)) { return; }
-    checkn = checkn.parentNode;
-  }
-  if (pn.className != "searchword") {
-        // word has not already been highlighted!
-        nv = node.nodeValue;
-        ni = tempNodeVal.indexOf(tempWordVal);
-        // Create a load of replacement nodes
-        before = document.createTextNode(nv.substr(0,ni));
-        docWordVal = nv.substr(ni,word.length);
-        after = document.createTextNode(nv.substr(ni+word.length));
-        hiwordtext = document.createTextNode(docWordVal);
-        hiword = document.createElement("mark");
-        hiword.className = "searchword";
-        hiword.appendChild(hiwordtext);
-        pn.insertBefore(before,node);
-        pn.insertBefore(hiword,node);
-        pn.insertBefore(after,node);
-        pn.removeChild(node);
-        count++;
-      }
-    };
-  }
+
+    // And do this node itself
+    if (node.nodeType == 3) { // text node
+        tempNodeVal = node.nodeValue.toLowerCase();
+        for (var i = 0; i < words.length; i++) {
+            tempWordVal = words[i].toLowerCase();
+            if (tempNodeVal.indexOf(tempWordVal) != -1) {
+                pn = node.parentNode;
+                // check if we're inside a "nosearchhi" zone
+                checkn = pn;
+                while (checkn.nodeType != 9 && checkn.nodeName.toLowerCase() != 'body') {
+                    // 9 = top of doc
+                    if (checkn.className.match(/\bnosearchhi\b/)) {
+                        return;
+                    }
+                    checkn = checkn.parentNode;
+                }
+                if (pn.className != "searchword") {
+                    // word has not already been highlighted!
+                    nv = node.nodeValue;
+                    ni = tempNodeVal.indexOf(tempWordVal);
+                    // Create a load of replacement nodes
+                    before = document.createTextNode(nv.substr(0, ni));
+                    docWordVal = nv.substr(ni, words[i].length);
+                    after = document.createTextNode(nv.substr(ni + words[i].length));
+                    hiwordtext = document.createTextNode(docWordVal);
+                    hiword = document.createElement("mark");
+                    hiword.className = "searchword";
+                    hiword.appendChild(hiwordtext);
+                    pn.insertBefore(before, node);
+                    pn.insertBefore(hiword, node);
+                    pn.insertBefore(after, node);
+                    pn.removeChild(node);
+                    count++;
+                }
+
+            }
+        }
+    }
 }
 
 chrome.storage.local.get('terms', function(store) {
-  searchFor(store.terms[0]);
-  for(var i = 0; i < store.terms.length; i++) {
-    searchFor(store.terms[i]);
-  }
+    searchFor(store.terms);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- ps_background.js
+++ ps_background.js
@@ -1,48 +1,65 @@
 var storage = chrome.storage.local;
 
 function addSearchTerm() {
-  var searchVal = search.value;
-  if(searchVal !== "") {
-    chrome.tabs.getSelected(null, function(tab) {
-      chrome.tabs.sendMessage(tab.id, {type: "search", term: searchVal}, function(response) {});
-    });
-    storage.get('terms', function(store) {
-      var terms = store.terms;
-      if(terms === undefined) terms = [];
-      terms.push(searchVal);
-      storage.set({'terms': terms}, function() {
-        // Notify that we saved.
-      });
-    });
-  }
+    var searchVal = search.value;
+    if (searchVal !== "") {
+        chrome.tabs.getSelected(null, function(tab) {
+            chrome.tabs.sendMessage(tab.id, {
+                type: "search",
+                term: searchVal
+            }, function(response) {});
+        });
+        storage.get('terms', function(store) {
+            var terms = store.terms;
+            if (terms === undefined) terms = [];
+            terms.push(searchVal);
+            storage.set({
+                'terms': terms
+            }, function() {
+                addListItem("termsList", store.terms);
+            });
+        });
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  saveSearch.addEventListener("click", addSearchTerm);
-  storage.get('terms', function(store) {
-    console.log(store);
-    //search.value = items.term;
-    for(var i = 0; i < store.length; i++) {
-          var element=document.getElementById('termsList').value;
-          
-          // This is the <ul id="myList"> element that will contains the new elements
-          var container = document.getElementById('termsList');
-          // Create a new <li> element for to insert inside <ul id="myList">
-          var new_element = document.createElement('li');
-          new_element.innerHTML = element;
-          container.insertBefore(new_element, container.firstChild);
-          // Show a message if the element has been added;
-          document.getElementById('msg').style.display="block";
-          document.getElementById('msg').innerHTML = "Elemend added!";
-          // Clean input field
-          //document..getElementById('newElement').value="";
-    }
-  });
+    saveSearch.addEventListener("click", addSearchTerm);
+    storage.get('terms', function(store) {
+        addListItem("termsList", store.terms);
+    });
 });
 
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     console.log(request);
-    if (request.type == "badge")
-      chrome.browserAction.setBadgeText({text: request.text});
+    if (request.type == "badge") chrome.browserAction.setBadgeText({
+        text: request.text
+    });
 });
+
+function addListItem(listID, terms) {
+    var element, new_element;
+    var container = document.getElementById(listID);
+    container.innerHTML = "";
+    for (var i = 0; i < terms.length; i++) {
+        element = document.getElementById(listID).value;
+        new_element = document.createElement('li');
+        new_element.innerHTML = element;
+        container.insertBefore(new_element, container.firstChild);
+        new_element.innerHTML = terms[i] + " (<span id='deleteTerm_" + i + "' termindex=" + i + " ><a href='#'>delete</a></span>)";
+        document.getElementById("deleteTerm_" + i).addEventListener("click", deleteTerm);
+    }
+
+}
+
+function deleteTerm(elm) {
+    var termIndex = this.getAttribute('termIndex');
+    storage.get('terms', function(store) {
+        var terms = store.terms;
+        terms.splice(termIndex, 1);
+        storage.set({
+            'terms': terms
+        }, function() {
+            addListItem("termsList", store.terms);
+        });
+    });
+}
